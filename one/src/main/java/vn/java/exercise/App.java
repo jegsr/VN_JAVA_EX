3dd077df2453cd462135da8cf37fab033a499e41
José Ribeiro <jose.r.ribeiro@oracle.com>
2022-09-04 14:13:33 +0100

    Java application to print all Integers

--- one/src/main/java/vn/java/exercise/App.java
+++ one/src/main/java/vn/java/exercise/App.java
@@ -1,14 +1,14 @@
 package vn.java.exercise;
 
-/**
- * Hello world!
- *
+/*
+ * Main Class responsible for the execution of the code.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        int startNumber = 1, maxNumber = 100;
+
+        //Printing all numbers from 1 to 100.
         PrintIntegers printClass = new PrintIntegers();
-        printClass.printAllIntegers(1,500);
+        printClass.printAllIntegers(startNumber, maxNumber);
     }
 }
--- one/src/main/java/vn/java/exercise/PrintIntegers.java
+++ one/src/main/java/vn/java/exercise/PrintIntegers.java
@@ -1,10 +1,23 @@
 package vn.java.exercise;
 
+/*
+ * Class responsible for printing all Integer numbers.
+ */
 public class PrintIntegers {
+    //Constants
     private String strDividedThree = "Visual";
     private String strDividedFive = "Nuts";
     private String strDividedBoth = "Visual Nuts";
 
+    /*
+     * Method responsible for printing all the Ingers from the @minNumber up to
+     * the @maxNumber.
+     * 
+     * If the integer is divided by 3 the String @strDividedThree will be printed,
+     * if divided by 5 the String @strDividedFive will be printed,
+     * if divided by both 3 and 5 the String @strDividedBoth will be printed,
+     * if none of the above then the Integer will be printed.
+     */
     public void printAllIntegers(int minNumber, int maxNumber) {
         for (int i = minNumber; i <= maxNumber; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
